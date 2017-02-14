4a274a07c1662f718f3a394e878310320d70c7bf
Daniyar Chambylov <dan.chambylov@gmail.com>
2017-02-14 10:12:45 +0600

    fix eslint errors

--- app/js/reducers/listView.js
+++ app/js/reducers/listView.js
@@ -1,16 +1,16 @@
-import {Map} from 'immutable';
-import {LIST_VIEW_TYPE} from "../actions/listView";
+import { Map } from 'immutable';
+import LIST_VIEW_TYPE from '../actions/listView';
 import * as listViewType from '../constants/listView';
 
 const initialState = Map({
-    listView: listViewType.LIST_VIEW_NORMAL
+  listView: listViewType.LIST_VIEW_NORMAL,
 });
 
-export default function(state = initialState, action) {
-    switch (action.type) {
-        case LIST_VIEW_TYPE:
-          state = state.update('listView', () => action.data);
-    }
-
-    return state;
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LIST_VIEW_TYPE:
+      return state.update('listView', () => action.data);
+    default:
+      return state;
+  }
 }
