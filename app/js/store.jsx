b09b6969b0dc375d978c37ffccd4e15f794fa007
Daniyar Chambylov <dan.chambylov@gmail.com>
2017-02-22 11:07:43 +0600

    integrate redux-persist

--- app/js/store.jsx
+++ app/js/store.jsx
@@ -2,6 +2,7 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import reducers from './reducers';
 
 const enhancers = [];
@@ -9,16 +10,21 @@ const enhancers = [];
 const loggerMiddleware = createLogger();
 
 enhancers.push(applyMiddleware(thunkMiddleware, loggerMiddleware));
+enhancers.push(autoRehydrate());
 
 if (process.env.NODE_ENV !== 'production') {
   const DevTools = require('./devtools').DevTools; // eslint-disable-line global-require
   enhancers.push(DevTools.instrument());
 }
 
-export default createStore(
+const store = createStore(
   combineReducers({
     routing: routerReducer,
     ...reducers,
   }),
   compose(...enhancers),
 );
+
+persistStore(store);
+
+export default store;
