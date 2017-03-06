1e23529a650bd49c5dd285560ee201cb67d23795
Daniyar Chambylov <dan.chambylov@gmail.com>
2017-03-06 09:05:13 +0600

    refactor history reducer

--- app/js/reducers/views/history.js
+++ app/js/reducers/views/history.js
@@ -15,29 +15,21 @@ const initialState = {
 
 function pushHistory(list, path) {
   const splitPath = path.split('/');
-  const newList = [...list];
 
-  let index = -1;
-  let elem = null;
-  if (splitPath.length < 3 || !/^[0-9]+$/.test(splitPath[2]) || !trackPaths.includes(splitPath[1])) {
-    return list;
+  if (splitPath.length === 3 && /^[0-9]+$/.test(splitPath[2]) && trackPaths.includes(splitPath[1])) {
+    const elem = +splitPath[2];
+    const newList = list.filter(x => x !== elem);
+
+    newList.unshift(elem);
+
+    if (newList.length > historyLength) {
+      newList.pop();
+    }
+
+    return newList;
   }
 
-  elem = +splitPath[2];
-
-  index = newList.indexOf(elem);
-
-  if (index > -1) {
-    newList.splice(index, 1);
-  }
-
-  newList.unshift(elem);
-
-  if (newList.length > historyLength) {
-    newList.pop();
-  }
-
-  return newList;
+  return list;
 }
 
 export default function history(state = initialState, action) {
