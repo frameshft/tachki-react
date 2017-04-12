9140b1b499c7c29f3e77548dfd55a07a010bf632
Daniyar Chambylov <dan.chambylov@gmail.com>
2017-04-12 14:09:49 +0600

    fix favorites

--- app/js/components/shared/utils.jsx
+++ app/js/components/shared/utils.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import PostItem from './post.item';
 
+function getEndponit(category) {
+  switch (category) {
+    case 'automobile':
+      return '/automobiles';
+    case 'spare':
+      return '/spare-parts';
+    case 'service':
+      return '/services';
+    case 'cargo':
+    default:
+      return '/cargo';
+  }
+}
+
 export function getPostComponent(item) {
-  return <PostItem key={ item.id } post={ item } />;
+  return <PostItem key={ item.id } post={ item } endpoint={ getEndponit(item.postType) } />;
 }
 
 export default true;
