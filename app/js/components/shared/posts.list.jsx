d1a677989de4cfc00433a0c7432656487e374ddf
frameshft <adiletkadyrov@gmail.com>
2017-05-30 13:33:58 +0600

    [#334] Change postslit h3 tag to h1

--- app/js/components/shared/posts.list.jsx
+++ app/js/components/shared/posts.list.jsx
@@ -427,7 +427,7 @@ class PostList extends React.Component {
         </Helmet>
 
         <div className='frontpage__block__head desktop'>
-          <h3 className='frontpage__block__title'>{ componentData.viewTitle }</h3>
+          <h1 className='frontpage__block__title'>{ componentData.viewTitle }</h1>
           { this.renderFrontpage() }
         </div>
 
