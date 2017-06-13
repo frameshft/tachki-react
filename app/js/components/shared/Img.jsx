a84985816105d888569ccc46667313c808e56092
frameshft <adiletkadyrov@gmail.com>
2017-06-13 14:40:55 +0600

    [#381] Add Img component

---
+++ app/js/components/shared/Img.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import classNames from 'classnames';
+import Spinner from '../shared/spinner';
+
+class Img extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onImageLoad = this.onImageLoad.bind(this);
+    this.state = { imgLoaded: false };
+  }
+
+  componentDidMount() {
+    const img = this.node.getBoundingClientRect();
+    if (img.complete) {
+      console.log('waat');
+      this.onImageLoad();
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.src !== this.props.src) {
+      console.log('WHATATATAT');
+      this.setState({ imgLoaded: false });
+    }
+  }
+
+  onImageLoad() {
+    if (!this.state.imgLoaded) {
+      this.setState({ imgLoaded: true });
+    }
+  }
+
+  render() {
+    return (
+      <div className={ classNames('image-wrapper', this.props.wrapperClasses) } onClick={ this.props.onClick }>
+        <img
+          className={ classNames('image-rendered', this.props.imgClasses, (this.state.imgLoaded) ? '' : 'hidden') }
+          src={ this.props.src }
+          alt={ this.props.alt }
+          onLoad={ this.onImageLoad }
+          ref={ (node) => { this.node = node; } }
+        />
+        <div className={ classNames('image-placeholder', this.props.placeholderClasses, (this.state.imgLoaded) ? 'hidden' : '') } >
+          { this.props.children || <Spinner />}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Img;
