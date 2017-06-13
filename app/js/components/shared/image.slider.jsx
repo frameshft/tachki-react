bcc2a802faf8a5904509f9d0df813733f222ac7a
frameshft <adiletkadyrov@gmail.com>
2017-06-13 15:51:53 +0600

    [#383] Change ImageSlider images

--- app/js/components/shared/image.slider.jsx
+++ app/js/components/shared/image.slider.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
+import Img from '../shared/Img';
 
 export default class extends React.Component {
   getImageDimensions(image) {
@@ -21,9 +22,10 @@ export default class extends React.Component {
     };
     const { images, title, refImage } = this.props;
     const { width, height } = this.getImageDimensions(refImage);
+    const imgTitle = title.toString();
     return (
       <Slider { ...settings }>
-        { images.map((x, i) => <div key={ i }><img className='slick-slide-image' src={ `${x}?w=${width}&h=${height}` } alt={ title } /></div>) }
+        { images.map((x, i) => <div key={ i }><Img imgClasses='slick-slide-image' src={ `${x}?w=${width}&h=${height}` } alt={ imgTitle } /></div>) }
       </Slider>
     );
   }
